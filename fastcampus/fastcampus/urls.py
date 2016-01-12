94c5dcfd531f70db390a1c1b36fa81cf990e0ada
Suchan An <dobestan@gmail.com>
2016-01-12 23:21:26 +0900

    update admin site_header: change name to "패스트코스"

--- fastcampus/fastcampus/urls.py
+++ fastcampus/fastcampus/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from django.contrib import admin
 
 
-admin.site.site_header = '패스트캠퍼스 관리자'
+admin.site.site_header = '패스트코스 관리자'
 
 urlpatterns = [
     url(r'^', admin.site.urls),
